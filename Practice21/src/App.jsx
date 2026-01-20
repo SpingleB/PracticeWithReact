5ad807e18c4f14fe1f27d5c6d5bd1438932ee579
SpingleB <spinglebreadgit@gmail.com>
2026-01-20 20:38:34 +0200

    asd

--- Practice21/src/App.jsx
+++ Practice21/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   }, []);
 
   useEffect(() => {
-    fetch(`../public/data.json`)
+    fetch(`/data.json`)
       .then((response) => {
         if (!response.ok) {
           throw new Error("Ошибка загрузки");
